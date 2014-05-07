f7f421443535b66f93922ee4a08a71388f7c76de
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2014-05-07 17:26:30 +0800

    remove apt-get upgrade; unattended-upgrades would do this better for us

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -1,17 +1,3 @@
 class swift::apt {
   
-    # We don't want to just apt-get upgrade willy nilly... so it is
-    # controlled by updating dummy file.
-    # http://projects.puppetlabs.com/projects/1/wiki/debian_patterns#Problem
-
-    exec { "/usr/bin/apt-get -y upgrade":
-      refreshonly => true,
-      timeout => 3600,
-      require => Exec['apt-get-update'],
-      subscribe   => File["/etc/do_apt_upgrade_storage"],
-    }
-
-    file { "/etc/do_apt_upgrade_storage":
-      source => "puppet:///modules/swift/do_apt_upgrade_storage",
-    }
 }
